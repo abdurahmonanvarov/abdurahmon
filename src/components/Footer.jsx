e74decf439a4bebfb4982c054bc19db48b077399
abdurahmonanvarov <dousolo9@gmail.com>
2025-01-10 12:50:25 +0500

    anvarov

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center py-5 text-slate-700">
-      © {new Date().getFullYear()} aawebdew.uz
+      © {new Date().getFullYear()} soloweb.uz
     </footer>
   );
 }
